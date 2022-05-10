24cda2554ede0e1b49b762d00affbd373f30b1f1
Rafa Patricio <rafaelpatricio91@gmail.com>
2022-05-10 07:09:21 -0300

    projeto criado e mysql configurado

---
+++ src/test/java/com/rafa/algamoney/api/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafa.algamoney.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

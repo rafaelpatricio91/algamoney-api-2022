28ef4142ed62db1ac8ef0da6347c6f6c50a1e8fa
Rafa Patricio <rafaelpatricio91@gmail.com>
2022-05-10 07:48:25 -0300

    migracao com flywaydb e get categorias

---
+++ src/main/java/com/rafa/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.rafa.algamoney.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafa.algamoney.api.model.Categoria;
+import com.rafa.algamoney.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+}

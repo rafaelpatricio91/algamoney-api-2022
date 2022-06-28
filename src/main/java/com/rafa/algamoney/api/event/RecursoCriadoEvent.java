1bcb4dcd33683579b0c91f3b0a7a73710bdc70f2
Rafa Patricio <rafaelpatricio91@gmail.com>
2022-06-28 20:29:39 -0300

    add header location com event e listener

---
+++ src/main/java/com/rafa/algamoney/api/event/RecursoCriadoEvent.java
@@ -0,0 +1,28 @@
+package com.rafa.algamoney.api.event;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationEvent;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	private static final long serialVersionUID = 6615148590796721873L;
+	
+	private HttpServletResponse response;
+	private Long codigo;
+
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
+		super(source);
+		this.response = response;
+		this.codigo = codigo;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+	
+}

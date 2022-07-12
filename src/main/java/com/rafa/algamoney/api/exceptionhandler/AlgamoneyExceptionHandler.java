16b80ab74099855e951b6f7f72de01cfe2a1312e
Rafa Patricio <rafaelpatricio91@gmail.com>
2022-07-12 08:13:40 -0300

    implementando delete de pessoa

--- src/main/java/com/rafa/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/rafa/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -41,6 +44,16 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		List<Erro> erros = criarListaErros(ex.getBindingResult());
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,WebRequest request) {
+		String msgUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String msgDeveloper = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(msgUsuario, msgDeveloper));
+		
+		return handleExceptionInternal(ex, erros, new HttpHeaders() ,HttpStatus.NOT_FOUND, request);
 	}
 	
 	private List<Erro> criarListaErros(BindingResult bindingResult) {

028cd3b669fe12a091a2851213139b835f96f678
Rafa Patricio <rafaelpatricio91@gmail.com>
2022-07-26 20:24:12 -0300

    busca lancamentos

---
+++ src/main/java/com/rafa/algamoney/api/resource/LancamentoResource.java
@@ -0,0 +1,31 @@
+package com.rafa.algamoney.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafa.algamoney.api.model.Lancamento;
+import com.rafa.algamoney.api.repository.LancamentoRepository;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@GetMapping
+	public List<Lancamento> listar() {
+		return lancamentoRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public Lancamento buscarPeloCodigo(@PathVariable Long codigo) {
+		return lancamentoRepository.findOne(codigo);
+	}
+	
+}

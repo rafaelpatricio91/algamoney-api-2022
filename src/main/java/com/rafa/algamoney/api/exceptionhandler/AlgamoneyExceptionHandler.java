1ab7ab16ac3a9be77809c1c90e6e46f61b5bb86c
Rafa Patricio <rafaelpatricio91@gmail.com>
2022-08-03 19:32:04 -0300

    tratando insert lancamento com pessoa ou categoria invalida

--- src/main/java/com/rafa/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/rafa/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -43,6 +45,15 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		List<Erro> erros = criarListaErros(ex.getBindingResult());
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex,WebRequest request) {
+		String msgUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String msgDeveloper = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> erros = Arrays.asList(new Erro(msgUsuario, msgDeveloper));
+		
+		return handleExceptionInternal(ex, erros, new HttpHeaders() ,HttpStatus.BAD_REQUEST, request);
 	}
 	
 	@ExceptionHandler({ EmptyResultDataAccessException.class })

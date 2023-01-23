746988abdb768d0d775e6cea5a0e9e50b3a67fab
Rafa Patricio <rafaelpatricio91@gmail.com>
2023-01-23 20:39:58 -0300

    spring profiles

--- src/main/java/com/rafa/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/rafa/algamoney/api/AlgamoneyApiApplication.java
@@ -2,8 +2,12 @@ package com.rafa.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.rafa.algamoney.api.config.properties.AlgamoneyApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rafa/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/rafa/algamoney/api/resource/LancamentoResource.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,7 @@ import com.rafa.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.rafa.algamoney.api.model.Lancamento;
 import com.rafa.algamoney.api.repository.LancamentoRepository;
 import com.rafa.algamoney.api.repository.filter.LancamentoFilter;
+import com.rafa.algamoney.api.repository.projection.ResumoLancamento;
 import com.rafa.algamoney.api.service.LancamentoService;
 
 @RestController
@@ -49,16 +51,25 @@ public class LancamentoResource {
 	private ApplicationEventPublisher publisher;
 
 	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Page<Lancamento> pesquisar(LancamentoFilter filter, Pageable pageable) {
 		return lancamentoRepository.filtrar(filter, pageable);
 	}
 	
+	@GetMapping(params = "resumo")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+	public Page<ResumoLancamento> resumir(LancamentoFilter filter, Pageable pageable) {
+		return lancamentoRepository.resumir(filter, pageable);
+	}
+	
 	@GetMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Lancamento buscarPeloCodigo(@PathVariable Long codigo) {
 		return lancamentoRepository.findOne(codigo); 
 	}
 	
 	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
 	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
 		Lancamento lancamentoSalvo = lancamentoService.criar(lancamento);
 		
@@ -68,6 +79,7 @@ public class LancamentoResource {
 	
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO') and #oauth2.hasScope('write')")
 	public void remover(@PathVariable Long codigo) {
 		lancamentoRepository.delete(codigo);
 	}
